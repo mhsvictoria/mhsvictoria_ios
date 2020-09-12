28d3d223abcc863cf4842cab8f8b0cc7d3d34441
Riley Peel <rpeel4@outlook.com>
2020-09-12 15:25:21 -0700

    Added age slider to form 1

--- mhsvictoria_ios/Form1ViewController.swift
+++ mhsvictoria_ios/Form1ViewController.swift
@@ -10,12 +10,63 @@ import UIKit
 
 class Form1ViewController: BaseViewController {
 
+    var age = 0 {
+        didSet {
+            setAgeFormLabel()
+        }
+    }
+    let ageFormLabel = UILabel()
+    let ageSlider = UISlider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadAge()
+        setSliderValue()
+        view.addSubview(ageFormLabel)
+        view.addSubview(ageSlider)
+        ageSlider.translatesAutoresizingMaskIntoConstraints = false
+        ageFormLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            ageSlider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            ageSlider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            ageSlider.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            ageFormLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 25),
+            ageFormLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            ageFormLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+        
+        ageSlider.addTarget(self, action: #selector(self.sliderValueChanged), for: .valueChanged)
         
     }
     
-
+    func setSliderValue() {
+        ageSlider.value = Float(age) / 100
+    }
+    
+    @objc func sliderValueChanged(sender: UISlider) {
+        age = Int(sender.value * 100)
+        saveAge()
+    }
+    
+    func setAgeFormLabel() {
+        ageFormLabel.text = "I am \(age) years old..."
+    }
+    
+    func saveAge() {
+        let preferences = UserDefaults.standard
+        preferences.set(age, forKey: "age")
+    }
+    
+    func loadAge() {
+        let preferences = UserDefaults.standard
+        if preferences.object(forKey: "age") != nil {
+            age = preferences.integer(forKey: "age")
+        } else {
+            age = 25
+        }
+    }
     /*
     // MARK: - Navigation
 
