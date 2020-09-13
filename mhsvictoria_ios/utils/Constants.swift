ff1c58c57470d3430458a33735b3e803341ce68a
Ron Allan <ron_allan@yahoo.com>
2020-09-12 17:30:15 -0700

    Added constants and a string file

---
+++ mhsvictoria_ios/utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  mhsvictoria_ios
+//
+//  Created by Ron Allan on 2020-09-12.
+//  Copyright © 2020 Ron Allan. All rights reserved.
+//
+
+import Foundation
+
+let ageKey = "age"
