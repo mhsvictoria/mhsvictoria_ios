ea2bea421026f42eea5b7b18235e6416e9e8804d
Ron Allan <ron_allan@yahoo.com>
2020-09-20 09:47:20 -0700

    Updated layout

--- mhsvictoria_ios/form/MainViewController.swift
+++ mhsvictoria_ios/form/MainViewController.swift
@@ -10,44 +10,21 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var header: UIView!
-    
     @IBOutlet weak var containerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = backgroundColor
-        header.backgroundColor = toolbarColor
     }
     
     override func viewWillLayoutSubviews() {
-        header.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            header.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            header.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            header.topAnchor.constraint(equalTo: view.topAnchor, constant: toolbarOffset),
-            header.heightAnchor.constraint(equalToConstant: toolbarHeight)
-        ])
-        let image = UIImage(named: "brain_sm.png")
-        let imageView = UIImageView(image: image)
-        header.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: header.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: header.centerYAnchor),
-            imageView.heightAnchor.constraint(equalTo: header.heightAnchor),
-            imageView.widthAnchor.constraint(equalTo: header.heightAnchor)
-        ])
-        
-        let yOffset = header.frame.minY + header.frame.height + margin
-        
+       
         containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
             containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
-            containerView.topAnchor.constraint(equalTo: view.topAnchor, constant: yOffset),
+            containerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100 +  pageControlHeight),
             containerView.heightAnchor.constraint(equalToConstant: view.frame.height/2)
         ])
     }
