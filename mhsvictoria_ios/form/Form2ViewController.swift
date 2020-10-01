9b537f1860db80dd879173a553c90f2da889d034
Riley Peel <rpeel4@outlook.com>
2020-10-01 14:57:15 -0700

    set up switches on form 2

--- mhsvictoria_ios/form/Form2ViewController.swift
+++ mhsvictoria_ios/form/Form2ViewController.swift
@@ -9,21 +9,71 @@
 import UIKit
 
 class Form2ViewController: BaseViewController {
-
+    
+    let switchToField: [UISwitch: String] = [UISwitch: String]()
+    var selectedFields: [String: Bool] = ["Field1": false, "Field2": true, "Field3": false]
+    let fieldNames = ["Field1", "Field2", "Field3", "Field4", "Field5", "Field6"]
+    let leftStackView = UIStackView()
+    let rightStackView = UIStackView()
+    let formHeaderLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        formHeaderLabel.text = "Select all that apply: "
+        view.addSubview(formHeaderLabel)
+        formHeaderLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            formHeaderLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
+            formHeaderLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            formHeaderLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin)
+        ])
+        view.addSubview(leftStackView)
+        view.addSubview(rightStackView)
+        setUpStackViews()
+        addSwitches()
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpStackViews() { //TODO refactor
+        leftStackView.axis = .vertical
+        leftStackView.distribution = .equalCentering
+        leftStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leftStackView.topAnchor.constraint(equalTo: formHeaderLabel.bottomAnchor, constant: margin),
+            leftStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -margin),
+            leftStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            leftStackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5, constant: -margin * 1.5)
+        ])
+        rightStackView.axis = .vertical
+        rightStackView.distribution = .equalCentering
+        rightStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            rightStackView.topAnchor.constraint(equalTo: formHeaderLabel.bottomAnchor, constant: margin),
+            rightStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -margin),
+            rightStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
+            rightStackView.leadingAnchor.constraint(equalTo: leftStackView.trailingAnchor, constant: margin)
+        ])
+        
     }
-    */
-
+    
+    func addSwitches() {
+        var switchesStackView = leftStackView
+        for (index, field) in fieldNames.enumerated() {
+            let fieldLabel = UILabel()
+            fieldLabel.text = field
+            let uiSwitch = UISwitch()
+            let switchContainer = UIStackView(arrangedSubviews: [fieldLabel, uiSwitch])
+            switchContainer.axis = .horizontal
+            switchContainer.distribution = .fillProportionally
+            switchesStackView.addArrangedSubview(switchContainer)
+            switchContainer.translatesAutoresizingMaskIntoConstraints = false
+            if index == (fieldNames.count / 2) - 1 {
+                switchesStackView = rightStackView
+            }
+        }
+    }
+    
+    @objc func switchPressed(sender: UISwitch) {
+        selectedFields[switchToField[sender]!] = sender.isOn
+    }
 }
