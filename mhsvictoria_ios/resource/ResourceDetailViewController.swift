7cf07d64cebf324e1ab08dfff5fafbbd22f4980b
Riley Peel <rileypeel@Rileys-MacBook-Pro.local>
2020-10-26 11:18:53 -0700

    added detail view for resources

---
+++ mhsvictoria_ios/resource/ResourceDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResourceDetailViewController.swift
+//  mhsvictoria_ios
+//
+//  Created by Riley Peel on 2020-10-24.
+//  Copyright © 2020 Ron Allan. All rights reserved.
+//
+
+import UIKit
+
+class ResourceDetailViewController: UIViewController {
+
+    var resourceData: Resource?
+    let resourceDetailView = ResourceDetailsView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        resourceDetailView.data = resourceData
+    }
+
+    func setUpView() {
+        view.backgroundColor = darkSlateGrey
+        view.addSubview(resourceDetailView)
+        resourceDetailView.layer.borderWidth = 1
+        resourceDetailView.layer.borderColor = UIColor(named: "gray")?.cgColor
+        resourceDetailView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            resourceDetailView.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
+            resourceDetailView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -margin),
+            resourceDetailView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+            resourceDetailView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin)
+        ])
+    }
+}
--- mhsvictoria_ios/resource/ResourceDetailsView.swift
+++ mhsvictoria_ios/resource/ResourceDetailsView.swift
@@ -10,12 +10,70 @@ import UIKit
 
 class ResourceDetailsView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var data: Resource? {
+        didSet {
+            setUpView()
+        }
     }
-    */
 
+    let titleLabel = UILabel()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setUpView() {
+        backgroundColor = lightSlateGrey
+        if let safeData = data {
+            titleLabel.textColor = .black
+            titleLabel.text = safeData.name
+            titleLabel.textAlignment = .center
+            addSubview(titleLabel)
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 200),
+                titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+                titleLabel.heightAnchor.constraint(equalToConstant: 30),
+                titleLabel.widthAnchor.constraint(equalToConstant: 200)
+            ])
+            
+        }
+        addIcons()
+    }
+    
+    func addIcons() {
+
+        let symbolConfig = UIImage.SymbolConfiguration(textStyle: .largeTitle)
+        let mail = UIImage(systemName: "envelope.fill", withConfiguration: symbolConfig)
+        let mailBtn = UIButton()
+        mailBtn.setImage(mail, for: .normal)
+        
+        
+        let phone = UIImage(systemName: "phone.fill", withConfiguration: symbolConfig)
+        let phoneBtn = UIButton()
+        phoneBtn.setImage(phone, for: .normal)
+
+        let help = UIImage(systemName: "questionmark.diamond.fill", withConfiguration: symbolConfig)
+        let helpBtn = UIButton()
+        helpBtn.setImage(help, for: .normal)
+        
+        let iconStackView = UIStackView()
+        iconStackView.distribution = .equalCentering
+        iconStackView.axis = .horizontal
+        addSubview(iconStackView)
+        iconStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            iconStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -margin),
+            iconStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: margin),
+            iconStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -margin),
+            iconStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        iconStackView.addArrangedSubview(phoneBtn)
+        iconStackView.addArrangedSubview(mailBtn)
+        iconStackView.addArrangedSubview(helpBtn)
+    }
 }
